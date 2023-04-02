0e47f760cdab9b72314d3fa34c41aebe94cb9b5b
José Garção <josemariagarcao15@gmail.com>
2023-04-02 21:59:44 +0100

    remove withdraw function. finish cancel recovery function. add docs.

--- contracts/SmartWallet.sol
+++ contracts/SmartWallet.sol
@@ -2,20 +2,67 @@
 pragma solidity 0.8.18;
 
 contract SmartWallet {
+    /******************************************************
+     *   STORAGE
+     ******************************************************/
+
+    /// @notice owner of the wallet
     address public owner;
+
+    /// @notice list of guardians
     address[] private guardians;
+
+    /// @notice true if guardian address, else false
     mapping(address => bool) public isGuardian;
 
+    /// @notice required votes to transfer ownership
     uint public requiredVotes;
+
+    /// @notice current recovery round
     uint public currentRecoveryRound;
+
+    /// @notice true if wallet is in recovery mode
     bool public inRecovery;
+
+    /// @notice struct used for bookkeeping during recovery mode
     struct Recovery {
         address candidate;
         uint recoveryRound;
         bool usedInExecuteRecovery;
     }
+
+    /// @notice mapping from guardian address to their Recovery struct
     mapping(address => Recovery) guardianToRecovery;
 
+    /******************************************************
+     *   MODIFIERS
+     ******************************************************/
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "caller is not owner");
+        _;
+    }
+
+    modifier onlyGuardian() {
+        require(isGuardian[msg.sender], "caller is not guardian");
+        _;
+    }
+
+    /******************************************************
+     *   EVENTS
+     ******************************************************/
+
+    /// TODO
+
+    /******************************************************
+     *   CONSTRUCTOR
+     ******************************************************/
+
+    /**
+     * @notice sets guardians and required votes
+     * @param _guardians - array of guardian addresses
+     * @param _requiredVotes - number of guardians required to transfer ownership
+     */
     constructor(address[] memory _guardians, uint _requiredVotes) {
         require(
             _requiredVotes <= _guardians.length,
@@ -31,23 +78,19 @@ contract SmartWallet {
         }
     }
 
+    /******************************************************
+     *   WALLET BASIC FUNCTIONALITIES
+     ******************************************************/
+
+    /// @notice allows deposit of ether
     receive() external payable {}
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "caller is not owner");
-        _;
-    }
-
-    modifier onlyGuardian() {
-        require(isGuardian[msg.sender], "caller is not guardian");
-        _;
-    }
-
-    function withdraw(uint _amount) external onlyOwner returns (bool) {
-        payable(msg.sender).transfer(_amount);
-        return true;
-    }
-
+    /**
+     * @notice allows transfer of ether to another account
+     * @param _to - address of the transfer recipient
+     * @param _amount - amount of ether to transfer
+     * @return - true if successfully transferred, else false
+     */
     function transferTo(
         address _to,
         uint _amount
@@ -60,10 +103,23 @@ contract SmartWallet {
         return true;
     }
 
+    /**
+     * @notice allows to retrieve the wallet balance
+     * @return - wallet balance
+     */
     function getBalance() external view returns (uint) {
         return address(this).balance;
     }
 
+    /******************************************************
+     *   GUARDIANS MANAGEMENT
+     ******************************************************/
+
+    /**
+     * @notice allows guardian addition
+     * @param _guardian - guardian address to be added
+     * @return - true if guardian added successfuly
+     */
     function addGuardian(address _guardian) public onlyOwner returns (bool) {
         require(!isGuardian[_guardian], "duplicate guardian");
         require(_guardian != address(0), "address can not be the zero address");
@@ -75,6 +131,11 @@ contract SmartWallet {
         return true;
     }
 
+    /**
+     * @notice allows guardian removal
+     * @param _guardian - guardian address to be removed
+     * @return - true if guardian removed successfuly
+     */
     function removeGuardian(
         address _guardian
     ) external onlyOwner returns (bool) {
@@ -87,10 +148,23 @@ contract SmartWallet {
         return true;
     }
 
+    /**
+     * @notice allows to retrieve guardians
+     * @return - array of guardians
+     */
     function getGuardians() external view returns (address[] memory) {
         return guardians;
     }
 
+    /******************************************************
+     *   RECOVERY
+     ******************************************************/
+
+    /**
+     * @notice allows trigger the recovery mode
+     * @param _candidate - address of the candidate
+     * @return - true if triggered the recovery mode successfuly
+     */
     function triggerRecovery(
         address _candidate
     ) public onlyGuardian returns (bool) {
@@ -104,6 +178,11 @@ contract SmartWallet {
         return true;
     }
 
+    /**
+     * @notice allows guardians to support the recovery process
+     * @param _candidate - address of the candidate
+     * @return - true if vote was saved successfuly
+     */
     function supportRecovery(
         address _candidate
     ) public onlyGuardian returns (bool) {
@@ -114,6 +193,12 @@ contract SmartWallet {
         return true;
     }
 
+    /**
+     * @notice allows guardians to close the recovery process
+     * @param _candidate - address of the candidate
+     * @param _guardians - array of guardian addresses that voted for the candidate
+     * @return - true if successfuly transfer ownership
+     */
     function executeRecovery(
         address _candidate,
         address[] calldata _guardians
@@ -146,21 +231,48 @@ contract SmartWallet {
         return true;
     }
 
+    /**
+     * @notice allows owner to cancel the recovery round
+     * @return - true if recovery round was successfuly canceled
+     */
     function cancelRecovery() public onlyOwner returns (bool) {
         require(inRecovery, "not in recovery mode");
+
         inRecovery = false;
+        ++currentRecoveryRound;
+
+        return true;
     }
 
+    /******************************************************
+     *   AUXILIAR FUNCTIONS
+     ******************************************************/
+
+    /**
+     * @notice save guardian's vote
+     * @param _guardian - guardian address
+     * @param _vote - guardian vote
+     */
     function saveVote(address _guardian, Recovery memory _vote) private {
         guardianToRecovery[_guardian] = _vote;
     }
 
+    /**
+     * @notice allows item removal from an array
+     * @param _arr - array of addresses from which an item should be removed
+     * @param _addr - item to be removed
+     */
     function removeFromArray(address[] storage _arr, address _addr) private {
         uint index = getIndexFromAddress(_arr, _addr);
         _arr[index] = _arr[_arr.length - 1];
         _arr.pop();
     }
 
+    /**
+     * @notice allows to retrieve the index of an array element
+     * @param arr - search array
+     * @param _address - address
+     */
     function getIndexFromAddress(
         address[] memory arr,
         address _address
